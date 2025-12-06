8224767c6787c9b0b3de91ae36317c5ac2670a58
Daniel Smith <d@danmasta.net>
2025-12-06 11:54:28 -0800

    add conninfo fn export

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ import { SYMBOLS } from './lib/constants.js';
 import { Request, UWSRequest } from './lib/request.js';
 import { UWSResponse } from './lib/response.js';
 import { serve, UWSServer } from './lib/server.js';
-import { RequestError, ResponseError, ServerError } from './lib/util.js';
+import { conninfo, RequestError, ResponseError, ServerError } from './lib/util.js';
 
 export {
+    conninfo,
     Request,
     RequestError,
     ResponseError,
