d627d199328656434267d39a88be18e51886bb61
Daniel Smith <d@danmasta.net>
2025-12-23 23:27:18 -0800

    update imports

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import { SYMBOLS } from './lib/constants.js';
-import { Request, UWSRequest } from './lib/request.js';
-import { UWSResponse } from './lib/response.js';
-import { serve, UWSServer } from './lib/server.js';
+import { Request, uWSRequest } from './lib/request.js';
+import { uWSResponse } from './lib/response.js';
+import { serve, uWSServer } from './lib/server.js';
 import { conninfo, RequestError, ResponseError, ServerError } from './lib/util.js';
 
 export {
@@ -12,9 +12,9 @@ export {
     serve,
     ServerError,
     SYMBOLS,
-    UWSRequest,
-    UWSResponse,
-    UWSServer
+    uWSRequest,
+    uWSResponse,
+    uWSServer
 };
 
 export default serve;
