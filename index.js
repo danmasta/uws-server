2096f72590000a6a1c6b7fe15e92726af65642f7
Daniel Smith <d@danmasta.net>
2026-02-08 16:14:04 -0800

    add serve static exports

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import { SYMBOLS } from './lib/constants.js';
 import { Request, uWSRequest } from './lib/request.js';
 import { uWSResponse } from './lib/response.js';
 import { serve, Server, uWSServer } from './lib/server.js';
+import { ServeStatic, serveStatic } from './lib/static.js';
 import { conninfo, RequestError, ResponseError, ServerError } from './lib/util.js';
 
 export {
@@ -12,6 +13,8 @@ export {
     serve,
     Server,
     ServerError,
+    ServeStatic,
+    serveStatic,
     SYMBOLS,
     uWSRequest,
     uWSResponse,
